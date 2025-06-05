8d6ed75be79f2021d6b77b79c7f8e9ee6b38db3a
Banumiah <banumiahpereira@gmail.com>
2025-06-05 20:13:44 +0100

    minha pagina web infra

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = "us-east-1" 
+}
+
+# Obter a VPC e subnets padrão
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Criar security group
+resource "aws_security_group" "e
